329af51041d06eea34fbba69ccc38c692cef452c
Christopher Ade W <111300719+caw083@users.noreply.github.com>
2024-07-01 05:04:52 +0700

    Add files via upload

---
+++ src/scripts/script_design.js
@@ -0,0 +1,18 @@
+// Menu
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
---
+++ webpack.common.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      // Existing rule for CSS files
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      // New rule for SCSS files
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/templates/index.html'),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/public/'),
+          to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+  ],
+};
